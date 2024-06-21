120d37dc5f1c6a9cffd751ac31a93cac505be949
hekemori <hekemor9@gmail.com>
2024-06-21 23:48:43 +0500

    Create const.py

---
+++ const.py
@@ -0,0 +1,11 @@
+# Вставьте токен бота / Enter your bot token
+TOKEN = ''
+
+# Напишите спам текст для краша / Write text for crash
+TEXT = '# @everyone @here'
+
+# Количество спам сообщений в текстовые каналы / Wrtie amount of spam text in text channels
+MESSAGE_COUNT = 
+
+# Количество каналов для создания / Write amount of channels creates
+CHANNEL_AND_ROLE_COUNT = 
